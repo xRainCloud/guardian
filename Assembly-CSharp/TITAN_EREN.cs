425541b89a6a4367b3ffc1afefda28f3037c2ca3
xRainCloud <79231076+xRainCloud@users.noreply.github.com>
2021-02-17 15:52:17 -0700

    Update TITAN_EREN.cs

--- Assembly-CSharp/TITAN_EREN.cs
+++ Assembly-CSharp/TITAN_EREN.cs
@@ -27,8 +27,8 @@ public class TITAN_EREN : Photon.MonoBehaviour
     private bool attackChkOnce;
     private float hitPause;
     public GameObject realBody;
-    public float lifeTime = 9999f;
-    private float lifeTimeMax = 9999f;
+    public float lifeTime = 99999f;
+    private float lifeTimeMax = 99999f;
     public bool rockLift;
     private bool hasDieSteam;
     private float dieTime;
